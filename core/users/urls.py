ccfdc67e76cbe0a6fce6300b4765e11251254a97
abrarfarhad <abrar.farhad.af@gmail.com>
2024-12-20 14:49:09 +0000

    auth handled

--- core/users/urls.py
+++ core/users/urls.py
@@ -1,6 +1,12 @@
 from rest_framework.authtoken import views
 from django.urls import path
+from users.views import (
+    RegisterUser,
+    GetUser,
+)
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
+    path('register/', RegisterUser.as_view()),
+    path('get-current-user/', GetUser.as_view()),
 ]
--- core/users/views.py
+++ core/users/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
-
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveAPIView,
+)
+from users.serializers import UserSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+class RegisterUser(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class GetUser(RetrieveAPIView):
+    serializer_class = UserSerializer
+    authentication_classes = [
+        TokenAuthentication,
+    ]
+    permission_classes = [
+        IsAuthenticated,
+    ]
+    queryset = User.objects.all()
+
+    def get_object(self):
+        return self.request.user
